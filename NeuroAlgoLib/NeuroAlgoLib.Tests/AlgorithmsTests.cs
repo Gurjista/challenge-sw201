b41ba4f879e074c18eae9f91bf814e2ecd413e25
Gustavo Rocha <gustavo.prietorocha1904@gmail.com>
2024-03-28 11:22:05 -0300

    Algoritmos e Testes
    Algoritmos implementados e testes corrigidos e aprovados

--- NeuroAlgoLib/NeuroAlgoLib.Tests/AlgorithmsTests.cs
+++ NeuroAlgoLib/NeuroAlgoLib.Tests/AlgorithmsTests.cs
@@ -28,17 +28,17 @@ public class AlgorithmsTests
         // TODO: Fix the missing values in the Is.EqualTo() calls
         Assert.Multiple(() =>
         {
-            Assert.That(Algorithms.FibonacciIterative(0), Is.EqualTo);
-            Assert.That(Algorithms.FibonacciIterative(1), Is.EqualTo);
-            Assert.That(Algorithms.FibonacciIterative(2), Is.EqualTo);
-            Assert.That(Algorithms.FibonacciIterative(3), Is.EqualTo);
-            Assert.That(Algorithms.FibonacciIterative(4), Is.EqualTo);
-            Assert.That(Algorithms.FibonacciIterative(5), Is.EqualTo);
-            Assert.That(Algorithms.FibonacciIterative(6), Is.EqualTo);
-            Assert.That(Algorithms.FibonacciIterative(7), Is.EqualTo);
-            Assert.That(Algorithms.FibonacciIterative(8), Is.EqualTo);
-            Assert.That(Algorithms.FibonacciIterative(9), Is.EqualTo);
-            Assert.That(Algorithms.FibonacciIterative(10), Is.EqualTo);
+            Assert.That(Algorithms.FibonacciIterative(0), Is.EqualTo(0));
+            Assert.That(Algorithms.FibonacciIterative(1), Is.EqualTo(1));
+            Assert.That(Algorithms.FibonacciIterative(2), Is.EqualTo(1));
+            Assert.That(Algorithms.FibonacciIterative(3), Is.EqualTo(2));
+            Assert.That(Algorithms.FibonacciIterative(4), Is.EqualTo(3));
+            Assert.That(Algorithms.FibonacciIterative(5), Is.EqualTo(5));
+            Assert.That(Algorithms.FibonacciIterative(6), Is.EqualTo(8));
+            Assert.That(Algorithms.FibonacciIterative(7), Is.EqualTo(13));
+            Assert.That(Algorithms.FibonacciIterative(8), Is.EqualTo(21));
+            Assert.That(Algorithms.FibonacciIterative(9), Is.EqualTo(34));
+            Assert.That(Algorithms.FibonacciIterative(10), Is.EqualTo(55));
         });
     }
 
@@ -46,7 +46,7 @@ public class AlgorithmsTests
     public void BinarySearchTest()
     {
         // TODO: Fix the wrong values in the Is.EqualTo() calls
-        int[] array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+        int[] array = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
         Assert.Multiple(() =>
         {
             Assert.That(Algorithms.BinarySearch(array, 1), Is.EqualTo(0));
@@ -58,13 +58,13 @@ public class AlgorithmsTests
             Assert.That(Algorithms.BinarySearch(array, 13), Is.EqualTo(6));
             Assert.That(Algorithms.BinarySearch(array, 15), Is.EqualTo(7));
             Assert.That(Algorithms.BinarySearch(array, 17), Is.EqualTo(8));
-            Assert.That(Algorithms.BinarySearch(array, 19), Is.EqualTo(8));
+            Assert.That(Algorithms.BinarySearch(array, 19), Is.EqualTo(9));
             Assert.That(Algorithms.BinarySearch(array, 0), Is.EqualTo(-1));
             Assert.That(Algorithms.BinarySearch(array, 2), Is.EqualTo(-1));
             Assert.That(Algorithms.BinarySearch(array, 4), Is.EqualTo(-1));
             Assert.That(Algorithms.BinarySearch(array, 6), Is.EqualTo(-1));
             Assert.That(Algorithms.BinarySearch(array, 8), Is.EqualTo(-1));
-            Assert.That(Algorithms.BinarySearch(array, 10), Is.EqualTo(1));
+            Assert.That(Algorithms.BinarySearch(array, 10), Is.EqualTo(-1));
             Assert.That(Algorithms.BinarySearch(array, 12), Is.EqualTo(-1));
             Assert.That(Algorithms.BinarySearch(array, 14), Is.EqualTo(-1));
             Assert.That(Algorithms.BinarySearch(array, 16), Is.EqualTo(-1));
--- NeuroAlgoLib/NeuroAlgoLib/Algorithms.cs
+++ NeuroAlgoLib/NeuroAlgoLib/Algorithms.cs
@@ -1,4 +1,6 @@
-﻿namespace NeuroAlgoLib;
+﻿using System;
+
+namespace NeuroAlgoLib;
 
 // TODO: Implement the Algorithms class
 
@@ -11,6 +13,11 @@ public static class Algorithms
     /// <returns>The nth Fibonacci number</returns>
     public static int FibonacciRecursive(int n)
     {
+        // Valores iniciais de fibonacci
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
         throw new NotImplementedException();
     }
 
@@ -21,6 +28,19 @@ public static class Algorithms
     /// <returns>The nth Fibonacci number</returns>
     public static int FibonacciIterative(int n)
     {
+        if (n == 0) return 0;
+
+        // Vetor para armazenar os resultados intermediários da sequência 
+        int[] vet = new int[n + 1];
+        vet[0] = 0;
+        vet[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            vet[i] = vet[i - 1] + vet[i - 2];
+        }
+
+        return vet[n];
         throw new NotImplementedException();
     }
 
@@ -32,6 +52,36 @@ public static class Algorithms
     /// <returns>Returns the index of the target value or -1 if not found.</returns>
     public static int BinarySearch(int[] array, int target)
     {
+        // Criando pontos de esquerda e direita da busca
+        int right = array.Length - 1;
+        int left = 0;
+        while (left <= right)
+        {
+            // Criando o ponto no meio do intervalo, para compara-lo com a cahve
+            int mid = (left + right) / 2;
+
+            // Checa se a chave está no meio do intervalo
+            if (array[mid] == target)
+            {
+                return mid;
+            }
+
+            // Se a chave é maior do que o valor no meio, ignora a parte da esquerda
+            if (array[mid] < target)
+            {
+                left = mid + 1;
+            }
+
+            // Se a chave é menor do que o valor no meio, ignora a parte da direita
+            else
+            {
+                right = mid - 1;
+            }
+
+        }
+        // Retorna -1 caso o valor não esteja presente
+        return -1;
+
         throw new NotImplementedException();
     }
 }
